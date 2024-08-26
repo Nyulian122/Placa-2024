a08c27380f33fbe99c30487bea211eced93599c9
Nyulian122 <103065477+Nyulian122@users.noreply.github.com>
2024-08-25 22:18:58 -0600

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,26 @@
+/*
+    let componentes = [
+    {
+        id: 'modal1',
+        boton: 'Disipador',
+        contenido: 'Información sobre el disipador.'
+    },
+    {
+        id: 'modal2',
+        boton: 'Ranuras de Memoria RAM',
+        contenido: 'Información sobre las ranuras de memoria RAM.'
+    },
+    {
+        id: 'modal3',
+        boton: 'Entrada De Alimentación',
+        contenido: 'Información sobre la entrada de alimentación.'
+    },
+    {
+        id: 'modal4',
+        boton: 'Botón 4',
+        contenido: 'Este es el botón 4.'
+    }
+];
+*/
+
+
---
+++ js/particles-config.js
@@ -0,0 +1,56 @@
+/* CONFIGURACIÓN DE PARTICLES.JS */
+particlesJS('particles-js',
+{
+    "particles": {
+        "number": {
+            "value": 100,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": ["#00FFFF", "#00CCCC", "#009999"]
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": true
+        },
+        "size": {
+            "value": 3,
+            "random": true
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#00FFFF",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out"
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            }
+        }
+    },
+    "retina_detect": true
+});
